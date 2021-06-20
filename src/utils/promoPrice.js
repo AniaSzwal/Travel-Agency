9a6219a2e949b6d804ac5d55bf86e14faaf7063c
Anna Szwal <aniaszwal@gmail.com>
2021-06-21 00:12:59 +0200

    Add promoPrice function to Trips

---
+++ src/utils/promoPrice.js
@@ -0,0 +1,10 @@
+export const promoPrice = (price, promo) => {
+  if (typeof(price) === 'string' && price.includes('$')){
+    price = price.slice(1).replace(',', '');
+    price = parseFloat(price);
+    let happyHourPrice = price - (price * promo / 100);
+    return ('$' + happyHourPrice);
+  }
+  price = parseFloat(price);
+  return price - (price * promo / 100);
+};
--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -1,4 +1,5 @@
 import { formatTime } from './formatTime';
+import { promoPrice } from './promoPrice';
 
 describe('utils', () => {
   describe('formatTime', () => {
@@ -22,6 +23,16 @@ describe('utils', () => {
       expect(formatTime(3793)).toBe('01:03:13');
       expect(formatTime(120)).toBe('00:02:00');
       expect(formatTime(3604)).toBe('01:00:04');
+    });
+  });
+
+  describe('promoPrice', () => {
+
+    it('should count price while HappyHour', () => {
+      expect(promoPrice(200, 20)).toBe(160);
+      expect(promoPrice(29318.86, 20)).toBe(23455.088);
+      expect(promoPrice('$200', 20)).toBe('$160');
+      expect(promoPrice('$29318.86', 20)).toBe('$23455.088');
     });
   });
 });
